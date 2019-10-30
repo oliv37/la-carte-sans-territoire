175e07cf78a59e0e7057bd979308c733e6c822ec
oliv37 <olivier.boisse@laposte.net>
2019-10-30 19:40:37 +0100

    Refacto backgroundImage HomeLink

--- src/components/HomeLink.js
+++ src/components/HomeLink.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 import styles from './HomeLink.module.css';
 
 function HomeLink({to, text, image}) {
-    const imageStyle = image && {"backgroundImage": `url(${image})`};
-    
-    return (
+	const backgroundImage = image ? `url(${image})` : 'none';
+
+	return (
         <Link to={to} className={styles.homeLink}>
-            <div className={styles.homeLinkImageContainer} style={imageStyle}/>
+            <div className={styles.homeLinkImageContainer} style={{backgroundImage}}/>
             <p className={styles.homeLinkText}>{text}</p>
         </Link>    
     );
